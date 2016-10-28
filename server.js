388801c05754e217f2af95d5e05c6daddbe52c66
Cedric Nicoloso <cedricnicoloso@qantas.com.au>
2016-10-28 18:12:37 +1100

    End of GraphQL part

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 import express from 'express';
+import schema from './data/schema'
+import GraphQLHTTP from 'express-graphql'
+
 import { MongoClient } from 'mongodb'
 
 let app = express();
-
 app.use(express.static('public'));
 
 let db;
 MongoClient.connect(process.env.MONGO_URL, (err, database) => {
   if (err) throw err;
   db = database;
+  app.use('/graphql', GraphQLHTTP({
+    schema: schema(db),
+    graphiql: true
+  }))
   app.listen(3000, () =>
     console.log('Now browse to localhost:3000/'));
 });
-
-app.get('/data/links', (req, res) => {
-  db.collection('links').find({}).toArray((err, links) => {
-    if (err) throw err;
-    res.json(links)
-  })
-})
