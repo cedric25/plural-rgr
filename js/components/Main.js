09f0621556216d2db4dff43b472c3b8123b44875
Cedric Nicoloso <cedricnicoloso@qantas.com.au>
2016-10-29 17:56:06 +1100

    End of "Working with variables"

--- js/components/Main.js
+++ js/components/Main.js
@@ -4,15 +4,28 @@ import Relay from 'react-relay'
 import Link from './Link'
 
 class Main extends React.Component {
+  setLimit = (e) => {
+    console.log('setLimit')
+    let newLimit = Number(e.target.value)
+    console.log(newLimit)
+    this.props.relay.setVariables({
+      limit: newLimit
+    })
+  }
   render() {
-    let content = this.props.store.links.map(link => {
+    let content = this.props.store.linkConnection.edges.map(edge => {
       return (
-        <Link key={link._id} link={link} />
+        <Link key={edge.node.id} link={edge.node} />
       )
     })
     return (
       <div>
         <h3>Links</h3>
+        Showing: <select onChange={this.setLimit} defaultValue="2">
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+
         <ul>
           {content}
         </ul>
@@ -23,12 +36,19 @@ class Main extends React.Component {
 
 // Declare the data requirement for this component
 Main = Relay.createContainer(Main, {
+  initialVariables: {
+    limit: 2
+  },
   fragments: {
     store: () => Relay.QL`
       fragment on Store {
-        links {
-          _id,
-          ${Link.getFragment('link')}
+        linkConnection(first: $limit) {
+          edges {
+            node {
+              id,
+              ${Link.getFragment('link')}  
+            }
+          }
         }
       }
       `
